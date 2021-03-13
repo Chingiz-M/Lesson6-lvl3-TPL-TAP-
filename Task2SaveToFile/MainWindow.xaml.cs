bbecc5688aaa50bfe8a9374c520f1b1f5f1c009b
Chingiz <chin1296@yandex.ru>
2021-03-13 19:45:16 +0300

    homework6

---
+++ Task2SaveToFile/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.IO;
+using System.Windows.f
+
+namespace Task2SaveToFile
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGo_Click(object sender, RoutedEventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                    System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+            }
+        }
+    }
+}
